1a11cd7507dd44a8778eb27d59c36079b28af4b7
Lukas Bai <bailu@bailu.ch>
2016-09-11 13:14:45 +0200

    Move project from ANT/Eclipse to Gradle/Android-Studio

---
+++ app/src/main/java/ch/bailu/aat/helpers/file/AbsContentAccess.java
@@ -0,0 +1,75 @@
+package ch.bailu.aat.helpers.file;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import ch.bailu.aat.helpers.AppLog;
+
+public abstract class AbsContentAccess {
+    public abstract InputStream open_r() throws FileNotFoundException;
+    public abstract OutputStream open_w() throws FileNotFoundException;
+
+
+    public String contentToString() throws IOException {
+        BufferedInputStream in = new BufferedInputStream(open_r());
+        StringBuilder out = new StringBuilder();
+
+        
+        try {
+            int c;
+            while ((c = in.read()) > -1) {
+                out.append((char)c);
+            }
+
+        } finally {
+            in.close();
+
+        }
+        return out.toString();
+    }
+
+
+
+
+    public void copy(File dest) throws Exception {
+        InputStream in = null;
+        OutputStream out = null;
+
+        try {
+
+            in = open_r();
+            out = new FileOutputStream(dest);
+            copy(in, out);
+            
+        } finally {
+            if (in != null) in.close();
+            if (out != null) out.close();
+        }
+    }
+
+    
+    private static void copy(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[4096];
+        int count;
+        while ((count = in.read(buffer)) > 0) {
+            out.write(buffer,0,count);
+        }
+    }
+
+
+   public abstract File toFile();
+    
+    
+    public long lastModified() {
+        File file = toFile();
+        if (file != null) {
+            return file.lastModified();
+        }
+        return System.currentTimeMillis();
+    }
+}
