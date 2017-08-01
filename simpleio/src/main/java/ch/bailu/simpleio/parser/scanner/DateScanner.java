eb9ce849279fb2caaf27cafb2a80bdeb65de3df8
Lukas Bai <bailu@bailu.ch>
2017-08-01 10:34:55 +0200

    Fix: Interpret time zone as UTC unless date ends with Z (Issue #14)

--- simpleio/src/main/java/ch/bailu/simpleio/parser/scanner/DateScanner.java
+++ simpleio/src/main/java/ch/bailu/simpleio/parser/scanner/DateScanner.java
@@ -3,62 +3,75 @@ package ch.bailu.simpleio.parser.scanner;
 import java.io.IOException;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 import ch.bailu.simpleio.io.Stream;
 
 
 public class DateScanner {
+    // Localtime to UTC fix
+    private final TimeZone localTimeZone;
+    private long localOffsetMillis;
+
     private final IntegerScanner minute;
     private final IntegerScanner hour;
     private final DoubleScanner seconds;
-    
+
     private long millis;
     private long dateBase;
     private int dateBuffer[] = new int[10];
 
     private final Stream stream;
-    
+
+
+
     public DateScanner(Stream s, long l) {
         stream=s;
-        
+
         millis=l;
         minute=new IntegerScanner(s);
         hour=new IntegerScanner(s);
         seconds = new DoubleScanner(s,3);
-        
+
         dateBase=0;
-        
+
         dateBuffer= new int[10];
         for (int i=0; i<dateBuffer.length; i++) dateBuffer[i]=0;
+
+        localTimeZone = TimeZone.getTimeZone(Calendar.getInstance().getTimeZone().getID());
     }
-    
+
     public void parse() throws IOException {
         boolean dateNeedsRescan=false;
-        
+
         stream.read();
         stream.skipWhitespace();
-        
+
         for (int i=0; i < dateBuffer.length; i++) {
             if (dateBuffer[i] != stream.get()) {
-                    dateBuffer[i]=stream.get();
-                    dateNeedsRescan=true;
+                dateBuffer[i]=stream.get();
+                dateNeedsRescan=true;
             }
             stream.read();
         }
-        
+
         if (dateNeedsRescan) scanDate();
         if (stream.haveA('T')) scanTime();
-        
+
+
+        if (stream.haveA('Z') == false) {
+            millis -= localOffsetMillis;
+        }
     }
-    
+
     public long getTimeMillis() {
         return millis;
     }
-    
+
     private void scanDate() {
         Calendar date;
-        
-        
+
+
         int list[]=new int[3];
         int x=0;
 
@@ -70,29 +83,26 @@ public class DateScanner {
                 list[x] += aDateBuffer - '0';
             }
         }
-        
+
         date = new GregorianCalendar(list[0],list[1]-1,list[2]); // year, month (zero-based), day
         dateBase = date.getTimeInMillis();
         millis=dateBase;
+
+        localOffsetMillis  = localTimeZone.getOffset(dateBase);
     }
-    
+
+
     private void scanTime() throws IOException {
-        
-        //stream.read();
         hour.scan();
-        //stream.read();
         minute.scan();
-        //stream.read();
         seconds.scan();
-        
-                
+
+
         millis=seconds.getInt();
         millis+=minute.getInteger()*60*1000;
         millis+=hour.getInteger()*60*60*1000;
-        
+
         millis+=dateBase;
     }
-    
-    
 
 }
