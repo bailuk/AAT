cf23ac0bbc65a1cfc19ed45241726368b25c65d4
bailuk <bailu@bailu.ch>
2018-10-30 21:19:35 +0100

    SolidBacklight: remove old (dark) mode

--- app/src/main/java/ch/bailu/aat/preferences/SolidBacklight.java
+++ app/src/main/java/ch/bailu/aat/preferences/SolidBacklight.java
@@ -10,30 +10,12 @@ public class SolidBacklight extends SolidStaticIndexList {
 
     private final static String KEY="backlight";
     
-    @SuppressWarnings("deprecation")
-    private final static int LOCK_MODE[] = {
-        PowerManager.PARTIAL_WAKE_LOCK, 
-        PowerManager.SCREEN_DIM_WAKE_LOCK,
-        PowerManager.SCREEN_BRIGHT_WAKE_LOCK
-        };
 
-    /**
-     * TODO change from PowerManager to Window flags
-     * This is the now official way of doing things
-     *  in Activity:
-     *  getWindow().addFlags(LOCK_FLAGS);
-     *
-     */
-    private final static int LOCK_FLAGS =
-            WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
-            WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
-            WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON;
 
     public SolidBacklight(Context c, int i) {
         super(c, KEY+i,
                 new String[] {
                     c.getString(R.string.p_backlight_off),
-                    c.getString(R.string.p_backlight_dim),
                     c.getString(R.string.p_backlight_bright)
                 }
             );
@@ -44,10 +26,6 @@ public class SolidBacklight extends SolidStaticIndexList {
     }
     
     
-    public int getLockMode() {
-        return LOCK_MODE[getIndex()];
-    }
-
     public boolean keepScreenOn() {
         return getIndex() != 0;
     }
