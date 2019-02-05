676ec05e158f4788114b9bacb4c2e5c0910c694f
bailuk <bailu@bailu.ch>
2019-02-05 20:24:29 +0100

    Use Android Tooltip view if possible

--- app/src/main/java/ch/bailu/aat/util/ui/ToolTip.java
+++ app/src/main/java/ch/bailu/aat/util/ui/ToolTip.java
@@ -1,9 +1,14 @@
 package ch.bailu.aat.util.ui;
 
+import android.os.Build;
 import android.view.View;
 
 public class ToolTip {
     public static void set(View view, Integer resID) {
-        new ToolTipLongClick(view, new ToolTipRes(view.getContext(), resID));
+        if (Build.VERSION.SDK_INT >= 26) {
+            view.setTooltipText(new ToolTipRes(view.getContext(), resID).getToolTip());
+        } else {
+            new ToolTipLongClick(view, new ToolTipRes(view.getContext(), resID));
+        }
     }
 }
