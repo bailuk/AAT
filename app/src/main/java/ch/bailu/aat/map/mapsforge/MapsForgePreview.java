fe769c5203789b3dd54bb175c9b703e36aa3940a
bailuk <bailu@bailu.ch>
2020-10-11 16:51:18 +0200

    MapsForgePreview: null frame buffer

--- app/src/main/java/ch/bailu/aat/map/mapsforge/MapsForgePreview.java
+++ app/src/main/java/ch/bailu/aat/map/mapsforge/MapsForgePreview.java
@@ -7,6 +7,7 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 
 import org.mapsforge.core.graphics.Canvas;
+import org.mapsforge.core.graphics.GraphicContext;
 import org.mapsforge.core.model.BoundingBox;
 import org.mapsforge.core.model.Dimension;
 import org.mapsforge.core.model.MapPosition;
@@ -121,10 +122,40 @@ public class MapsForgePreview extends MapsForgeViewBase {
 
     @Override
     public FrameBuffer getFrameBuffer() {
-        return new FrameBuffer(getModel().frameBufferModel, getModel().displayModel, AndroidGraphicFactory.INSTANCE) {
+        return new FrameBuffer() {
+            @Override
+            public void adjustMatrix(float diffX, float diffY, float scaleFactor, Dimension mapViewDimension, float pivotDistanceX, float pivotDistanceY) {
+
+            }
+
+            @Override
+            public void destroy() {
+
+            }
+
+            @Override
+            public void draw(GraphicContext graphicContext) {
+
+            }
+
+            @Override
+            public void frameFinished(MapPosition frameMapPosition) {
+
+            }
+
+            @Override
+            public Dimension getDimension() {
+                return null;
+            }
+
             @Override
             public org.mapsforge.core.graphics.Bitmap getDrawingBitmap() {
                 return null;
+            }
+
+            @Override
+            public void setDimension(Dimension dimension) {
+
             }
         };
     }
