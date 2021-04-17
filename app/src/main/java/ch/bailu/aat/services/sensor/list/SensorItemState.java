bailuk/AAT

commit 61cb8b8d7b2a6a447759cfeb1eb2a6b1f0416dd4

Add even more strings to strings.xml

app/src/main/java/ch/bailu/aat/services/sensor/list/SensorItemState.java
@@ -1,6 +1,8 @@
 package ch.bailu.aat.services.sensor.list;
 
-import ch.bailu.aat.util.ToDo;
+import android.content.Context;
+
+import ch.bailu.aat.R;
 
 public class SensorItemState {
     public final static int UNSCANNED = 0;
@@ -16,14 +18,14 @@ public class SensorItemState {
     // configuration, discovery: unscanned, supported, enabled, unsupported
     // from real time status : scanning, connecting, connected, disconnected trying to reconnect?
 
-    private static final String[] STATE_DESCRIPTION = {
-            ToDo.translate("Unscanned"),
-            ToDo.translate("Scanning..."),
-            ToDo.translate("Supported"),
-            ToDo.translate("Not connected"),
-            ToDo.translate("Connecting..."),
-            ToDo.translate("Connected"),
-            ToDo.translate("Not supported")
+    private static final int[] STATE_DESCRIPTION = {
+            R.string.sensor_state_unscanned,
+            R.string.sensor_state_scanning,
+            R.string.sensor_state_supported,
+            R.string.sensor_state_not_connected,
+            R.string.sensor_state_connecting,
+            R.string.sensor_state_not_connected,
+            R.string.sensor_state_not_supported
     };
 
     private int state;
@@ -107,7 +109,7 @@ public class SensorItemState {
     }
 
 
-    public String getSensorStateDescription() {
-        return STATE_DESCRIPTION[state];
+    public String getSensorStateDescription(Context c) {
+        return c.getString(STATE_DESCRIPTION[state]);
     }
 }

app/src/main/java/ch/bailu/aat/description/SensorStateDescription.java
@@ -2,10 +2,10 @@ package ch.bailu.aat.description;
 
 import android.content.Context;
 
-import ch.bailu.aat.gpx.attributes.GpxAttributes;
+import ch.bailu.aat.R;
 import ch.bailu.aat.gpx.GpxInformation;
+import ch.bailu.aat.gpx.attributes.GpxAttributes;
 import ch.bailu.aat.services.sensor.list.SensorList;
-import ch.bailu.aat.util.ToDo;
 
 public class SensorStateDescription extends StateDescription {
     private String unit = "";
@@ -17,7 +17,7 @@ public class SensorStateDescription extends StateDescription {
 
     @Override
     public String getLabel() {
-        return ToDo.translate("Sensors");
+        return getContext().getString(R.string.sensor_state);
     }
 
 

app/src/main/java/ch/bailu/aat/preferences/location/SolidAdjustGpsAltitudeValue.java
@@ -2,8 +2,8 @@ package ch.bailu.aat.preferences.location;
 
 import android.content.Context;
 
+import ch.bailu.aat.R;
 import ch.bailu.aat.preferences.general.SolidUnit;
-import ch.bailu.aat.util.ToDo;
 
 public class SolidAdjustGpsAltitudeValue extends SolidAltitude {
     public static final String KEY = "AltitudeCorrection";
@@ -21,6 +21,6 @@ public class SolidAdjustGpsAltitudeValue extends SolidAltitude {
 
     @Override
     public String getLabel() {
-        return addUnit(ToDo.translate("GPS altitude modification"));
+        return addUnit(getString(R.string.p_adjust_altitude_by));
     }
 }

app/src/main/java/ch/bailu/aat/preferences/map/SolidMapsForgeMapFile.java
@@ -4,7 +4,7 @@ import android.content.Context;
 
 import java.util.ArrayList;
 
-import ch.bailu.aat.util.ToDo;
+import ch.bailu.aat.R;
 import ch.bailu.util_java.foc.Foc;
 
 public class SolidMapsForgeMapFile extends SolidMapsForgeDirectory {
@@ -15,7 +15,7 @@ public class SolidMapsForgeMapFile extends SolidMapsForgeDirectory {
 
     @Override
     public String getLabel() {
-        return ToDo.translate("Offline map");
+        return getString(R.string.p_offline_map);
     }
 
     @Override

app/src/main/java/ch/bailu/aat/preferences/map/SolidOsmFeaturesList.java
@@ -3,10 +3,10 @@ package ch.bailu.aat.preferences.map;
 
 import android.content.Context;
 
+import ch.bailu.aat.R;
 import ch.bailu.aat.preferences.SolidBoolean;
 import ch.bailu.aat.services.cache.CacheService;
 import ch.bailu.aat.services.cache.osm_features.ObjMapFeatures;
-import ch.bailu.aat.util.ToDo;
 import ch.bailu.aat.util.filter_list.KeyList;
 
 public class SolidOsmFeaturesList extends SolidBoolean {
@@ -20,7 +20,7 @@ public class SolidOsmFeaturesList extends SolidBoolean {
 
     @Override
     public String getLabel() {
-        return ToDo.translate("All");
+        return getString(R.string.all);
     }
 
 

app/src/main/java/ch/bailu/aat/services/background/DownloadTask.java
@@ -1,6 +1,7 @@
 package ch.bailu.aat.services.background;
 
 import android.content.Context;
+
 import androidx.annotation.NonNull;
 
 import java.io.IOException;
@@ -12,7 +13,6 @@ import java.net.URL;
 import ch.bailu.aat.services.ServiceContext;
 import ch.bailu.aat.util.AppBroadcaster;
 import ch.bailu.aat.util.ContextWrapperInterface;
-import ch.bailu.aat.util.ToDo;
 import ch.bailu.aat.util.net.URX;
 import ch.bailu.aat.util.ui.AppLog;
 import ch.bailu.aat.util.ui.AppString;
@@ -101,7 +101,6 @@ public class DownloadTask extends FileTask implements ContextWrapperInterface{
 
 
             while (( count = input.read(buffer)) != -1) {
-                //throwOrContinue();
 
                 total+=count;
                 output.write(buffer, 0, count);
@@ -110,18 +109,11 @@ public class DownloadTask extends FileTask implements ContextWrapperInterface{
 
         } finally {
             Foc.close(output);
-            //if (connection != null) connection.disconnect();
             Foc.close(input);
         }
 
         return total;
     }
-
-
-    public void throwOrContinue() throws IOException {
-        if (! super.canContinue()) throw new IOException(ToDo.translate("Cancel"));
-    }
-
 
 
 

app/src/main/java/ch/bailu/aat/services/sensor/bluetooth_le/BleSensorSDK18.java
@@ -9,6 +9,7 @@ import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothProfile;
 import android.content.Context;
 import android.os.Build;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 
@@ -17,9 +18,9 @@ import java.util.List;
 import ch.bailu.aat.gpx.GpxInformation;
 import ch.bailu.aat.services.ServiceContext;
 import ch.bailu.aat.services.sensor.SensorInterface;
+import ch.bailu.aat.services.sensor.list.SensorItemState;
 import ch.bailu.aat.services.sensor.list.SensorList;
 import ch.bailu.aat.services.sensor.list.SensorListItem;
-import ch.bailu.aat.services.sensor.list.SensorItemState;
 import ch.bailu.aat.util.Timer;
 import ch.bailu.aat.util.ui.AppLog;
 
@@ -229,7 +230,7 @@ public final class BleSensorSDK18 extends BluetoothGattCallback implements Senso
                 + "@"
                 + device.getAddress()
                 + ":"
-                + item.getSensorStateDescription();
+                + item.getSensorStateDescription(context);
     }
 
 

app/src/main/java/ch/bailu/aat/services/sensor/internal/InternalSensorSDK23.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 
@@ -62,7 +63,7 @@ public abstract class InternalSensorSDK23 implements SensorEventListener, Sensor
     @NonNull
     @Override
     public String toString() {
-        return getName() + "@" + getAddress() + ":" + item.getSensorStateDescription();
+        return getName() + "@" + getAddress() + ":" + item.getSensorStateDescription(context);
     }
 
 

commit b77bcc91f0fe3843015db4a2ab2cac11a0d00f16

SensorItemState: fix string for CONNECTED
Fixes regression by commit 61cb8b8d7b2a6a447759cfeb1eb2a6b1f0416dd4
which caused the UI to display "Not connected" instead of "Connected".

app/src/main/java/ch/bailu/aat/services/sensor/list/SensorItemState.java
@@ -24,7 +24,7 @@ public class SensorItemState {
             R.string.sensor_state_supported,
             R.string.sensor_state_not_connected,
             R.string.sensor_state_connecting,
-            R.string.sensor_state_not_connected,
+            R.string.sensor_state_connected,
             R.string.sensor_state_not_supported
     };
 