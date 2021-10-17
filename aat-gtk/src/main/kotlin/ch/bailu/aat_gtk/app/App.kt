d0191c8a387ff199fc4ce9526cd25114c0ba4cf5
bailuk <bailu@bailu.ch>
2021-10-17 07:23:30 +0200

    aat-gtk hello world

--- aat-gtk/src/main/kotlin/ch/bailu/aat_gtk/app/App.kt
+++ aat-gtk/src/main/kotlin/ch/bailu/aat_gtk/app/App.kt
@@ -1,5 +1,35 @@
 package ch.bailu.aat_gtk.app
 
+import ch.bailu.gtk.GTK
+import ch.bailu.gtk.gio.ApplicationFlags
+import ch.bailu.gtk.gtk.Application
+import ch.bailu.gtk.gtk.ApplicationWindow
+import ch.bailu.gtk.gtk.Button
+import ch.bailu.gtk.type.Str
+import ch.bailu.gtk.type.Strs
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    GTK.init();
+
+    val app = Application(
+        Str("ch.bailu.aat_gtk"),
+        ApplicationFlags.FLAGS_NONE
+    )
+
+    app.onActivate {
+
+        // Create a new window
+        val window = ApplicationWindow(app)
+
+        // Create a new button
+        val button =
+            Button.newWithLabelButton(Str("Hello, World!"))
+
+        // When the button is clicked, close the window
+        button.onClicked { window.close() }
+        window.add(button)
+        window.showAll()
+    }
+
+    app.run(args.size, Strs(args))
 }
