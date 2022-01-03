31c7f380025591e1da4fa66b94df1b48a93f64b8
bailuk <bailu@bailu.ch>
2022-01-03 09:25:50 +0100

    Reorganize app startup

--- aat-gtk/src/main/kotlin/ch/bailu/aat_gtk/app/App.kt
+++ aat-gtk/src/main/kotlin/ch/bailu/aat_gtk/app/App.kt
@@ -1,8 +1,6 @@
 package ch.bailu.aat_gtk.app
 
-import ch.bailu.aat_gtk.dispatcher.GtkBroadcaster
 import ch.bailu.aat_gtk.logger.SL4JLogger
-import ch.bailu.aat_gtk.service.GtkServices
 import ch.bailu.aat_gtk.solid.GtkStorage
 import ch.bailu.aat_gtk.ui.window.MainWindow
 import ch.bailu.aat_lib.app.AppConfig
@@ -11,39 +9,22 @@ import ch.bailu.aat_lib.dispatcher.AppBroadcaster
 import ch.bailu.aat_lib.logger.AppLog
 import ch.bailu.aat_lib.logger.BroadcastLogger
 import ch.bailu.aat_lib.map.Attachable
-import ch.bailu.aat_lib.service.ServicesInterface
 import ch.bailu.gtk.GTK
 import ch.bailu.gtk.gio.ActionMap
+import ch.bailu.gtk.gio.ApplicationFlags
 import ch.bailu.gtk.gtk.Application
 import ch.bailu.gtk.gtk.ApplicationWindow
 import ch.bailu.gtk.type.Str
-import ch.bailu.gtk.type.Strs
 import org.mapsforge.map.gtk.graphics.GtkGraphicFactory
 import kotlin.system.exitProcess
 
-fun main(args: Array<String>) {
+fun main() {
     GTK.init()
-
-    val app = Application(Str(GtkAppConfig.applicationId), 0)
-
-    app.onActivate {
-        try {
-            val window = MainWindow(ActionMap(app.cast()), ApplicationWindow(app), App.services, App.storage, App.broadcaster)
-            App.attach(window)
-
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
-    app.run(args.size, Strs(args))
+    App.run()
 }
 
 
 object App {
-    val storage = GtkStorage()
-    val broadcaster = GtkBroadcaster()
-    val services: ServicesInterface
-
     private val attachables = ArrayList<Attachable>()
 
     init {
@@ -53,24 +34,37 @@ object App {
 
         AppLog.setError(
             BroadcastLogger(
-                broadcaster,
+                GtkAppContext.broadcaster,
                 AppBroadcaster.LOG_ERROR,
                 SL4JLogger()
             )
         )
         AppLog.setInfo(
             BroadcastLogger(
-                broadcaster,
+                GtkAppContext.broadcaster,
                 AppBroadcaster.LOG_INFO,
                 SL4JLogger()
             )
         )
-        services = GtkServices(storage, broadcaster)
     }
 
+    fun run() {
+        val app = Application(Str(GtkAppConfig.applicationId), ApplicationFlags.FLAGS_NONE)
+
+        app.onActivate {
+            try {
+                val window = MainWindow(ActionMap(app.cast()), ApplicationWindow(app), GtkAppContext.services, GtkAppContext.broadcaster)
+                attach(window)
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+        app.run(0, null)
+    }
 
     fun exit(exitCode: Int) {
-        services.trackerService.onStartStop()
+        GtkAppContext.services.trackerService.onStartStop()
 
         attachables.forEach { it.onDetached() }
         GtkStorage.save()
