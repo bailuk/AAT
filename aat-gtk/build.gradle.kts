a4c169180ed807189a409fbc917bfca8fa8f1ef6
bailuk <bailu@bailu.ch>
2022-01-03 09:29:34 +0100

    Update kotlin

--- aat-gtk/build.gradle.kts
+++ aat-gtk/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     application
 
     // https://kotlinlang.org/docs/gradle.html#targeting-the-jvm
-    kotlin("jvm") version "1.5.31"
+    kotlin("jvm") version "1.6.10"
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_11
