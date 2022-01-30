b5ef46d4ef416c34526bf0f5323f909d0bcffa1d
bailuk <bailu@bailu.ch>
2022-01-30 08:58:02 +0100

    Extract Layouter

--- aat-android/src/main/java/ch/bailu/aat/views/description/CockpitView.java
+++ aat-android/src/main/java/ch/bailu/aat/views/description/CockpitView.java
@@ -2,58 +2,65 @@ package ch.bailu.aat.views.description;
 
 
 import android.content.Context;
-import android.view.View;
 import android.view.ViewGroup;
 
-import ch.bailu.aat_lib.description.AltitudeConfigurationDescription;
-import ch.bailu.aat_lib.description.CadenceDescription;
-import ch.bailu.aat_lib.description.HeartRateDescription;
-import ch.bailu.aat_lib.description.PowerDescription;
-import ch.bailu.aat_lib.dispatcher.DispatcherInterface;
+import java.util.ArrayList;
+
 import ch.bailu.aat.preferences.Storage;
 import ch.bailu.aat.preferences.location.SolidProvideAltitude;
 import ch.bailu.aat.util.ui.UiTheme;
+import ch.bailu.aat_lib.description.AltitudeConfigurationDescription;
+import ch.bailu.aat_lib.description.CadenceDescription;
 import ch.bailu.aat_lib.description.ContentDescription;
+import ch.bailu.aat_lib.description.HeartRateDescription;
+import ch.bailu.aat_lib.description.PowerDescription;
+import ch.bailu.aat_lib.dispatcher.DispatcherInterface;
 import ch.bailu.aat_lib.gpx.InfoID;
 import ch.bailu.aat_lib.preferences.StorageInterface;
+import ch.bailu.aat_lib.view.cockpit.Layouter;
 
 
 public class CockpitView extends ViewGroup {
 
-    private final Layouter layouter = new Layouter();
+    private final Layouter layouter;
 
     private final UiTheme theme;
     private final StorageInterface storage;
+
+    private final ArrayList<ContentDescription> contentDescriptions = new ArrayList<>();
 
     public CockpitView(Context context, UiTheme theme) {
         super(context);
         this.theme = theme;
         this.storage = new Storage((context));
         theme.background(this);
+
+        layouter = new Layouter(contentDescriptions,
+                (index, x, y, w, h) -> getChildAt(index).layout(x,y,w,h));
     }
 
 
-    public void add(DispatcherInterface di, ContentDescription de) {
-        add(di, de, InfoID.TRACKER);
+    public NumberView add(DispatcherInterface di, ContentDescription de) {
+        return add(di, de, InfoID.TRACKER);
     }
 
 
-    public void addC(DispatcherInterface di, ContentDescription de, int... iid) {
-        final NumberView v = new ColorNumberView(getContext(), de, theme);
-
-        addView(v);
-        di.addTarget(v, iid);
+    public NumberView addC(DispatcherInterface di, ContentDescription de, int... iid) {
+        return _addView(di, new ColorNumberView(getContext(), de, theme), iid);
     }
 
 
     public NumberView add(DispatcherInterface di, ContentDescription de, int... iid) {
-        final NumberView v = new NumberView(getContext(),de, theme);
-
-        addView(v);
-        di.addTarget(v, iid);
-        return v;
+        return _addView(di, new NumberView(getContext(),de, theme), iid);
     }
 
+
+    private NumberView _addView(DispatcherInterface di, NumberView v, int... iid) {
+        addView(v);
+        di.addTarget(v, iid);
+        contentDescriptions.add(v.getDescription());
+        return v;
+    }
 
 
     public void addAltitude(DispatcherInterface di) {
@@ -76,132 +83,10 @@ public class CockpitView extends ViewGroup {
         v.requestOnClickSensorReconnect();
     }
 
-
-
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         if (changed) {
             layouter.layout(r-l,b-t);
         }
-
-    }
-
-    private class Layouter {
-        private static final int MAX_CHARS_PER_LINE=50;
-        private int parent_width=0;
-        private int parent_height=0;
-
-        private final Placer placer = new Placer();
-        private final Placer realPlacer = new RealPlacer();
-
-
-        public void layout(int w, int h) {
-            int chars = 1;
-
-            parent_width = w;
-            parent_height = h;
-
-            while (chars < MAX_CHARS_PER_LINE) {
-                if (tryPlacement(chars)) {
-                    doPlacement(chars);
-                    break;
-                } else chars++;
-            }
-        }
-
-
-
-
-        private boolean tryPlacement(int charsPerLine) {
-            placer.place(charsPerLine);
-            return placeItems(placer);
-        }
-
-        private boolean doPlacement(int charsPerLine) {
-            realPlacer.place(charsPerLine);
-            return placeItems(realPlacer);
-        }
-
-        private boolean placeItems(Placer p) {
-            boolean works=true;
-
-            final int size = getChildCount();
-            for (int i=0; i< size && works; i++) {
-                works = p.placeItem(i);
-            }
-            return works;
-        }
-
-        private class Placer {
-            private static final int MIN_CHARS = 4;
-            private static final int RATIO=3;
-            private int char_width, char_height, xpos, ypos;
-
-
-            public void place(int charsPerLine) {
-                xpos=ypos=0;
-                calculateCharGeometry(charsPerLine);
-            }
-
-            private void calculateCharGeometry(int charsPerLine) {
-                char_width=parent_width/charsPerLine;
-
-                char_height=char_width*RATIO;
-                if (char_height > parent_height) {
-                    char_height=parent_height;
-                    char_width=char_height/RATIO;
-                }
-            }
-
-
-            public boolean placeItem(int index) {
-                boolean works=true;
-                int width=getWidthOfView(index);
-
-                if (width>parent_width) {
-                    works=false;
-                } else if (width+xpos > parent_width) {
-                    works=addLine();
-                    if (works) works = placeItem(index);
-                } else {
-                    setGeometry(index, width, char_height);
-                    xpos += width;
-                }
-                return works;
-            }
-
-            protected void setGeometry(int index, int width, int height) {}
-
-            protected int getXPos() {return xpos;}
-            protected int getYPos() {return ypos;}
-
-            private boolean addLine() {
-                ypos+=char_height;
-                xpos=0;
-                return ((char_height + ypos) <= parent_height);
-            }
-
-
-            private int getWidthOfView(int index) {
-                final NumberView child = (NumberView) getChildAt(index);
-
-                int len= child.getDescription().getValue().length();
-                len = Math.max(len, MIN_CHARS);
-                return len*char_width;
-            }
-        }
-
-
-        private class RealPlacer extends Placer {
-
-
-            @Override
-            protected void setGeometry(int index, int width, int height) {
-                final View child = getChildAt(index);
-
-                child.layout(getXPos(),getYPos(), getXPos()+width, getYPos()+height);
-            }
-        }
-
     }
 }
---
+++ aat-lib/src/main/java/ch/bailu/aat_lib/view/cockpit/Layouter.java
@@ -0,0 +1,124 @@
+package ch.bailu.aat_lib.view.cockpit;
+
+import java.util.List;
+
+import ch.bailu.aat_lib.description.ContentDescription;
+
+public class Layouter {
+    private static final int MAX_CHARS_PER_LINE=50;
+    private int parent_width=0;
+    private int parent_height=0;
+
+    private final Placer placer = new Placer((index, x, y, w, h) -> {});
+    private final Placer realPlacer;
+
+    private final List<ContentDescription> items;
+
+
+    public Layouter(List<ContentDescription> items, DoPlacement doPlacement) {
+        this.items = items;
+        this.realPlacer = new Placer(doPlacement);
+    }
+
+    public void layout(int w, int h) {
+        int chars = 1;
+
+        parent_width = w;
+        parent_height = h;
+
+        while (chars < MAX_CHARS_PER_LINE) {
+            if (tryPlacement(chars)) {
+                doPlacement(chars);
+                break;
+            } else chars++;
+        }
+    }
+
+
+    private boolean tryPlacement(int charsPerLine) {
+        placer.place(charsPerLine);
+        return placeItems(placer);
+    }
+
+    private boolean doPlacement(int charsPerLine) {
+        realPlacer.place(charsPerLine);
+        return placeItems(realPlacer);
+    }
+
+    private boolean placeItems(Placer p) {
+        boolean works=true;
+
+        final int size = items.size();
+        for (int i=0; i< size && works; i++) {
+            works = p.placeItem(i);
+        }
+        return works;
+    }
+
+    private class Placer {
+        private static final int MIN_CHARS = 4;
+        private static final int RATIO=3;
+        private int char_width, char_height, xpos, ypos;
+
+        private final DoPlacement doPlacement;
+
+        public Placer(DoPlacement doPlacement) {
+            this.doPlacement = doPlacement;
+        }
+
+        public void place(int charsPerLine) {
+            xpos=ypos=0;
+            calculateCharGeometry(charsPerLine);
+        }
+
+        private void calculateCharGeometry(int charsPerLine) {
+            char_width=parent_width/charsPerLine;
+
+            char_height=char_width*RATIO;
+            if (char_height > parent_height) {
+                char_height=parent_height;
+                char_width=char_height/RATIO;
+            }
+        }
+
+        public boolean placeItem(int index) {
+            boolean works=true;
+            int width=getWidthOfView(index);
+
+            if (width>parent_width) {
+                works=false;
+            } else if (width+xpos > parent_width) {
+                works=addLine();
+                if (works) works = placeItem(index);
+            } else {
+                setGeometry(index, width, char_height);
+                xpos += width;
+            }
+            return works;
+        }
+
+        protected void setGeometry(int index, int width, int height) {
+            doPlacement.setGeometry(index, getXPos(),getYPos(), getXPos()+width, getYPos()+height);
+        }
+
+        protected int getXPos() {return xpos;}
+        protected int getYPos() {return ypos;}
+
+        private boolean addLine() {
+            ypos+=char_height;
+            xpos=0;
+            return ((char_height + ypos) <= parent_height);
+        }
+
+
+        private int getWidthOfView(int index) {
+            int len= items.get(index).getValue().length();
+            len = Math.max(len, MIN_CHARS);
+            return len*char_width;
+        }
+    }
+
+    public interface DoPlacement {
+        void setGeometry(int index, int x, int y, int w, int h);
+    }
+}
