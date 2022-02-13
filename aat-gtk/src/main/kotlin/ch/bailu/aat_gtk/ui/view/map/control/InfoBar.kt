99b7fd32db78cc557fc34909227950484bc11bea
bailuk <bailu@bailu.ch>
2022-02-13 07:56:04 +0100

    Test TileStackMenu

--- aat-gtk/src/main/kotlin/ch/bailu/aat_gtk/ui/view/map/control/InfoBar.kt
+++ aat-gtk/src/main/kotlin/ch/bailu/aat_gtk/ui/view/map/control/InfoBar.kt
@@ -1,6 +1,7 @@
 package ch.bailu.aat_gtk.ui.view.map.control
 
 import ch.bailu.aat_gtk.app.GtkAppContext
+import ch.bailu.aat_gtk.solid.GtkMapDirectories
 import ch.bailu.aat_gtk.ui.view.menu.MapMenu
 import ch.bailu.aat_gtk.ui.view.menu.MapQueryMenu
 import ch.bailu.aat_gtk.ui.view.menu.PopupButton
@@ -8,8 +9,10 @@ import ch.bailu.aat_gtk.ui.view.menu.model.LocationMenu
 import ch.bailu.aat_gtk.ui.view.solid.SolidMenuButton
 import ch.bailu.aat_lib.map.MapContext
 import ch.bailu.aat_lib.preferences.StorageInterface
+import ch.bailu.aat_lib.preferences.map.MapDirectories
 import ch.bailu.aat_lib.preferences.map.SolidLegend
 import ch.bailu.aat_lib.preferences.map.SolidMapGrid
+import ch.bailu.aat_lib.preferences.map.SolidMapTileStack
 import ch.bailu.gtk.gtk.Orientation
 import ch.bailu.gtk.helper.ActionHelper
 
@@ -22,5 +25,6 @@ class InfoBar(actionHelper: ActionHelper, mcontext: MapContext, storage: Storage
         add(SolidMenuButton(actionHelper, SolidLegend(storage, mcontext.solidKey)).overlay)
         add(PopupButton(actionHelper, MapQueryMenu()).apply { setIcon("search") }.overlay)
         add(PopupButton(actionHelper, LocationMenu()).apply { setIcon("location") }.overlay)
+        add(SolidMenuButton(actionHelper, SolidMapTileStack(GtkMapDirectories(storage, GtkAppContext).createSolidRenderTheme())).overlay)
     }
 }
