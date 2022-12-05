bca98034c78348395d908e070c5895619fe50587
bailuk <bailu@bailu.ch>
2022-12-05 22:11:35 +0100

    Clean code

--- aat-gtk/src/main/kotlin/ch/bailu/aat_gtk/view/menu/PopupButton.kt
+++ aat-gtk/src/main/kotlin/ch/bailu/aat_gtk/view/menu/PopupButton.kt
@@ -3,12 +3,10 @@ package ch.bailu.aat_gtk.view.menu
 import ch.bailu.aat_gtk.config.Layout
 import ch.bailu.aat_gtk.lib.extensions.setIcon
 import ch.bailu.aat_gtk.view.menu.provider.MenuProvider
-import ch.bailu.aat_lib.logger.AppLog
 import ch.bailu.gtk.gtk.Application
 import ch.bailu.gtk.gtk.Button
 import ch.bailu.gtk.gtk.Overlay
 import ch.bailu.gtk.gtk.PopoverMenu
-import ch.bailu.gtk.type.Str
 
 open class PopupButton(app: Application, menuProvider: MenuProvider) {
     private val button = Button()
@@ -20,17 +18,14 @@ open class PopupButton(app: Application, menuProvider: MenuProvider) {
         overlay.child = button
         overlay.addOverlay(popover)
 
-        menuProvider.createActions(app) // Todo: is this the right place?
-
+        menuProvider.createActions(app)
         PopoverMenu(popover.cast()).apply {
             menuProvider.createCustomWidgets().forEach {
                 addChild(it.widget, it.id)
             }
         }
 
-        button.onClicked {
-            popover.popup()
-        }
+        button.onClicked { popover.popup() }
     }
 
     fun setIcon(resource: String, size: Int = Layout.iconSize) {
