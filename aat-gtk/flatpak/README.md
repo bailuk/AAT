ba29467fae6a70a6f91d4e6bd2e905f53cc9f07c
bailuk <bailu@bailu.ch>
2023-05-29 14:16:16 +0200

    GTK: Add flatpak configuration and scripts

---
+++ aat-gtk/flatpak/README.md
@@ -0,0 +1,33 @@
+
+# Create Flatpak
+
+```bash
+# generate 'flatpak/gradle-sources.json'
+./flatpak-generate-gradle-sources.sh
+
+# create and run flatpak distribution
+./flatpak-create.sh
+```
+
+# Flatpak and gradle
+
+Flatpak builder runs gradle inside a container with restrictive internet access.
+Downloading of dependencies is not possible.
+[Maven/Gradle support](https://github.com/flatpak/flatpak-builder-tools/issues/37)
+
+Solution:
+[Run Gradle generator script](https://github.com/flatpak/flatpak-builder-tools/pull/276)
+
+# Flatpak commands
+
+```bash
+# Open shell inside runtime
+flatpak run org.gnome.Sdk//44
+
+```
+
+# Flatpak examples
+
+- [gtk-meteo](https://github.com/bailuk/gtk-meteo/blob/main/flatpak/README.md)
+- [Apostrophe](https://github.com/flathub/org.gnome.gitlab.somas.Apostrophe/blob/master/org.gnome.gitlab.somas.Apostrophe.json)
+- [Ghidra](https://github.com/flathub/org.ghidra_sre.Ghidra)
---
+++ aat-gtk/flatpak/flatpak-create.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+test -d flatpak || cd ..
+cd flatpak || exit 1
+
+mkdir -p build || exit 1
+
+export LANG=C # So we can google error messages
+
+echo "_"
+echo "Install dependencies"
+flatpak install flathub org.gnome.Platform//44 org.gnome.Sdk//44 org.freedesktop.Sdk.Extension.openjdk17/x86_64/22.08 || exit 1
+
+echo "_"
+echo "Build flatpak"
+flatpak-builder --force-clean build/build ch.bailu.aat.json || exit 1
+
+echo "_"
+echo "Install flatpak"
+echo "flatpak-builder --user --install --force-clean build/build ch.bailu.aat.json || exit 1"
+
+# Single-file bundle
+# https://unix.stackexchange.com/questions/695934/how-do-i-build-a-flatpak-package-file-from-a-flatpak-manifest
+# https://docs.flatpak.org/en/latest/single-file-bundles.html
+
+echo "_"
+echo "Export flatpak"
+flatpak build-export build/export build/build || exit 1
+
+
+echo "_"
+echo "Build single-file bundle"
+flatpak build-bundle build/export build/aat.flatpak "ch.bailu.aat"
+
+echo "_"
+echo "Install single-file bundle"
+echo "> flatpak install build/aat.flatpak"
+
+echo "_"
+echo "Run flatpak"
+echo "> flatpak run ch.bailu.aat"
