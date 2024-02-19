b6973a42a2e3ab75d365bf75df9059ab7ec56eb9
Lukas Bai <bailu@bailu.ch>
2024-02-19 14:32:02 +0100

    Fix color values

--- aat-lib/src/main/java/ch/bailu/aat_lib/app/AppColor.kt
+++ aat-lib/src/main/java/ch/bailu/aat_lib/app/AppColor.kt
@@ -1,9 +1,9 @@
 package ch.bailu.aat_lib.app
 
 object AppColor {
-    const val HL_ORANGE = -0x9a00
-    const val HL_GREEN = -0x330100
-    const val HL_BLUE = -0xff2701
+    const val HL_ORANGE = 0xFFFF6600.toInt()
+    const val HL_GREEN = 0xFFCCFF00.toInt()
+    const val HL_BLUE = 0xFF00D8FF.toInt()
 
     /**
      * 0xAARRGGBB
@@ -14,13 +14,9 @@ object AppColor {
      */
     @JvmField
     val OVERLAY_COLOR = intArrayOf(
-        -0x6c61,
-        -0x6b03,
-        -0x6b0049,
-        -0x2006c,
-        -0x6600c0ac,
-        -0x6600c004,
-        -0x66c00083,
-        -0x660300c1
+        0xFFFF939F.toInt(),
+        0xFFFF94fD.toInt(),
+        0xFF94FFB7.toInt(),
+        0xFFFDFF94.toInt(),
     )
 }
