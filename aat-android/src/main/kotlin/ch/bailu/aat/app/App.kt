feadec727e413e8156392eed286fb87e2011a75a
bailuk <bailu@bailu.ch>
2024-04-06 14:25:01 +0200

    Fix ACRA report

--- aat-android/src/main/kotlin/ch/bailu/aat/app/App.kt
+++ aat-android/src/main/kotlin/ch/bailu/aat/app/App.kt
@@ -7,11 +7,10 @@ import ch.bailu.aat_lib.app.AppConfig
 import ch.bailu.aat_lib.app.AppGraphicFactory
 import ch.bailu.aat_lib.logger.AppLog
 import ch.bailu.aat_lib.logger.BroadcastLoggerFactory
-import org.acra.ACRA
-import org.acra.config.CoreConfigurationBuilder
-import org.acra.config.DialogConfigurationBuilder
-import org.acra.config.MailSenderConfigurationBuilder
+import org.acra.config.dialog
+import org.acra.config.mailSender
 import org.acra.data.StringFormat
+import org.acra.ktx.initAcra
 import org.mapsforge.core.util.Parameters
 import org.mapsforge.map.android.graphics.AndroidGraphicFactory
 
@@ -21,7 +20,7 @@ class App : Application() {
         initLogger()
         initMapsForge()
         if (AppConfig.getInstance().isRelease) {
-            initAcra()
+            initAcraReport()
         }
         super.onCreate()
     }
@@ -46,22 +45,20 @@ class App : Application() {
         Parameters.SQUARE_FRAME_BUFFER = false
     }
 
-    private fun initAcra() {
-        ACRA.init(this, CoreConfigurationBuilder()
-            .withBuildConfigClass(org.acra.BuildConfig::class.java)
-            .withReportFormat(StringFormat.KEY_VALUE_LIST)
-            .withPluginConfigurations(MailSenderConfigurationBuilder()
-                .withMailTo(AppConfig.getInstance().appContact)
-                .withEnabled(true)
-                .build())
-            .withPluginConfigurations(DialogConfigurationBuilder()
-                .withTitle("${AppConfig.getInstance().appName} crashed")
-                .withText("""
+    private fun initAcraReport() {
+        initAcra {
+            buildConfigClass = org.acra.BuildConfig::class.java
+            reportFormat = StringFormat.KEY_VALUE_LIST
+            dialog {
+                title = "${AppConfig.getInstance().appName} crashed"
+                text = """
                     This will open your e-mail app to send a crash report including some information about your device to "${AppConfig.getInstance().appContact}".
                     This will help the author to fix and improve this app.
-                """.trimIndent())
-                .withEnabled(true)
-                .build())
-        )
+                """.trimIndent()
+            }
+            mailSender {
+                mailTo = AppConfig.getInstance().appContact
+            }
+        }
     }
 }
