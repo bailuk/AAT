54394d2b62abeb9d148bbdc7eb8a1edeaebe104b
bailuk <bailu@bailu.ch>
2024-04-09 12:03:12 +0200

    Pin commit

--- aat-gtk/flatpak/flatpak-pin-commit.sh
+++ aat-gtk/flatpak/flatpak-pin-commit.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
 out_file="ch.bailu.aat.json"
-in_file="${out_appstream}.in"
 changelogs="../../metadata/en-US/changelogs"
 git_hash=$(git show --format="%H" --no-patch)
 
 echo "_"
 echo "update '${out_file}' (commit: $git_hash)"
-sed -ie 's/"branch": .*"/"branch": '\"${git_hash}\"'/g' "${out_file}"
+sed -i 's/"branch": .*"/"branch": '\"${git_hash}\"'/g' "${out_file}"
