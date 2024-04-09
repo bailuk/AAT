d5ebaea04899414c6d08cb2e4c2e7ae4ac2c379e
bailuk <bailu@bailu.ch>
2024-04-09 19:24:20 +0200

    GTK/Android: Improve getGpxDirectories helper function

--- aat-android/src/main/kotlin/ch/bailu/aat/util/ui/AppSelectDirectoryDialog.kt
+++ aat-android/src/main/kotlin/ch/bailu/aat/util/ui/AppSelectDirectoryDialog.kt
@@ -4,30 +4,31 @@ import android.app.AlertDialog
 import android.content.Context
 import android.content.DialogInterface
 import ch.bailu.aat.R
-import ch.bailu.aat.preferences.system.AndroidSolidDataDirectoryDefault
-import ch.bailu.aat_lib.preferences.system.SolidDataDirectory
+import ch.bailu.aat_lib.app.AppContext
 import ch.bailu.aat_lib.util.fs.AppDirectory
 import ch.bailu.foc.Foc
-import ch.bailu.foc_android.FocAndroidFactory
 
-abstract class AppSelectDirectoryDialog(private val context: Context, private val srcFile: Foc) {
-    private val directories: Array<Foc>
+abstract class AppSelectDirectoryDialog(appContext: AppContext, context: Context, private val srcFile: Foc) {
+    private val directories = AppDirectory.getGpxDirectories(appContext)
 
     init {
-        val sdirectory = SolidDataDirectory(AndroidSolidDataDirectoryDefault(context), FocAndroidFactory(context))
-        directories = AppDirectory.getGpxDirectories(sdirectory)
-        val names = arrayOfNulls<String>(directories.size)
-        for (i in names.indices) names[i] = directories[i].name
-        val dialog = AlertDialog.Builder(context)
-        dialog.setTitle(srcFile.name + ": " + context.getString(R.string.file_copy))
+        AlertDialog.Builder(context).apply {
+            setTitle(srcFile.name + ": " + context.getString(R.string.file_copy))
 
-        dialog.setItems(names) { dialogInterface: DialogInterface, index: Int ->
-            copyTo(context, srcFile, directories[index])
-            dialogInterface.dismiss()
+            setItems(toNameArray(directories)) { dialogInterface: DialogInterface, index: Int ->
+                copyTo(context, srcFile, directories[index].file)
+                dialogInterface.dismiss()
+            }
+
+            create()
+            show()
         }
+    }
 
-        dialog.create()
-        dialog.show()
+    private fun toNameArray(directories: ArrayList<AppDirectory.GpxDirectoryEntry>): Array<String> {
+        return Array(directories.size) {
+            directories[it].name
+        }
     }
 
     abstract fun copyTo(context: Context, srcFile: Foc, destDirectory: Foc)
