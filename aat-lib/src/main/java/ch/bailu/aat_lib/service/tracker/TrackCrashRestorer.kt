c224f03476f0867c0ba1e8c985ed3f39d19b0b06
bailuk <bailu@bailu.ch>
2024-07-13 18:17:14 +0200

    Port to Kotlin

--- aat-lib/src/main/java/ch/bailu/aat_lib/service/tracker/TrackCrashRestorer.kt
+++ aat-lib/src/main/java/ch/bailu/aat_lib/service/tracker/TrackCrashRestorer.kt
@@ -1,55 +1,39 @@
-package ch.bailu.aat_lib.service.tracker;
+package ch.bailu.aat_lib.service.tracker
 
-import java.io.IOException;
+import ch.bailu.aat_lib.gpx.GpxList
+import ch.bailu.aat_lib.gpx.attributes.AutoPause
+import ch.bailu.aat_lib.logger.AppLog
+import ch.bailu.aat_lib.preferences.system.SolidDataDirectory
+import ch.bailu.aat_lib.resources.Res.str
+import ch.bailu.aat_lib.service.tracker.TrackLogger.Companion.generateTargetFile
+import ch.bailu.aat_lib.util.fs.AppDirectory.getLogFile
+import ch.bailu.aat_lib.xml.parser.gpx.GpxListReader
+import ch.bailu.aat_lib.xml.writer.GpxListWriter
+import ch.bailu.foc.Foc
+import java.io.IOException
 
-import ch.bailu.aat_lib.gpx.GpxList;
-import ch.bailu.aat_lib.gpx.attributes.AutoPause;
-import ch.bailu.aat_lib.preferences.system.SolidDataDirectory;
-import ch.bailu.aat_lib.resources.Res;
-import ch.bailu.aat_lib.xml.writer.GpxListWriter;
-import ch.bailu.aat_lib.xml.parser.gpx.GpxListReader;
-import ch.bailu.aat_lib.logger.AppLog;
-import ch.bailu.aat_lib.util.fs.AppDirectory;
-import ch.bailu.foc.Foc;
-
-public final class TrackCrashRestorer{
-
-    public TrackCrashRestorer (SolidDataDirectory sdirectory, int presetIndex) throws IOException {
-
-        Foc source = AppDirectory.getLogFile(sdirectory);
-
+class TrackCrashRestorer(sdirectory: SolidDataDirectory, presetIndex: Int) {
+    init {
+        val source = getLogFile(sdirectory)
         if (source.exists()) {
-
-            GpxList track = readFile(source);
-            if (track.getPointList().size() > TrackLogger.MIN_TRACK_POINTS) {
-                // TODO message to gui
-                AppLog.i(this, Res.str().tracker_restore());
-
-                retstoreFile(sdirectory,track, presetIndex);
-
+            val track = readFile(source)
+            if (track.pointList.size() > TrackLogger.MIN_TRACK_POINTS) {
+                AppLog.i(this, str().tracker_restore())
+                restoreFile(sdirectory, track, presetIndex)
             }
-            source.rm();
+            source.rm()
         }
-
     }
 
-
-    private void retstoreFile(SolidDataDirectory sdirectory, GpxList track, int presetIndex) throws IOException {
-        Foc target = TrackLogger.generateTargetFile(sdirectory, presetIndex);
-
-        GpxListWriter writer=new GpxListWriter(track,target);
-        writer.close();
-
+    @Throws(IOException::class)
+    private fun restoreFile(sdirectory: SolidDataDirectory, track: GpxList, presetIndex: Int) {
+        val target = generateTargetFile(sdirectory, presetIndex)
+        val writer = GpxListWriter(track, target)
+        writer.close()
     }
 
-
-
-
-
-
-
-    private GpxList readFile(Foc remainingLogFile) {
-        GpxListReader reader = new GpxListReader(remainingLogFile, AutoPause.NULL);
-        return reader.getGpxList();
+    private fun readFile(remainingLogFile: Foc): GpxList {
+        val reader = GpxListReader(remainingLogFile, AutoPause.NULL)
+        return reader.gpxList
     }
 }
