a7cb73857cfca97fe8e5c517ca89550553bab00a
bailuk <bailu@bailu.ch>
2024-09-01 14:12:05 +0200

    EditorStatusLabel Ux fixes

--- aat-gtk/src/main/kotlin/ch/bailu/aat_gtk/view/map/control/EditorStatusLabel.kt
+++ aat-gtk/src/main/kotlin/ch/bailu/aat_gtk/view/map/control/EditorStatusLabel.kt
@@ -2,35 +2,25 @@ package ch.bailu.aat_gtk.view.map.control
 
 import ch.bailu.aat_gtk.config.Layout
 import ch.bailu.aat_gtk.config.Strings
-import ch.bailu.aat_gtk.lib.extensions.margin
 import ch.bailu.aat_lib.dispatcher.TargetInterface
 import ch.bailu.aat_lib.gpx.information.GpxInformation
+import ch.bailu.aat_lib.gpx.information.InfoID
 import ch.bailu.aat_lib.gpx.information.InformationUtil
 import ch.bailu.gtk.gtk.Align
 import ch.bailu.gtk.gtk.Box
 import ch.bailu.gtk.gtk.Label
 import ch.bailu.gtk.gtk.Orientation
-import ch.bailu.gtk.type.Str
 
 class EditorStatusLabel : TargetInterface {
-    private val defaultNameLabel = Label(Str.NULL).apply {
-        addCssClass("status-label-bold")
-    }
-    private val fileNameLabel = Label(Str.NULL).apply {
-        addCssClass("status-label")
-    }
-    private val statusLabel = Label(Str.NULL).apply {
+    private val fileNameLabel = Label(InformationUtil.defaultName(InfoID.EDITOR_OVERLAY)).apply {
         addCssClass("status-label")
     }
 
     val box = Box(Orientation.HORIZONTAL, Layout.margin).apply {
         addCssClass(Strings.mapControl)
-        margin(Layout.margin)
         valign = Align.END
         halign = Align.CENTER
-        append(defaultNameLabel)
         append(fileNameLabel)
-        append(statusLabel)
     }
 
     fun hide() {
@@ -42,7 +32,12 @@ class EditorStatusLabel : TargetInterface {
     }
 
     override fun onContentUpdated(iid: Int, info: GpxInformation) {
-        defaultNameLabel.setLabel(InformationUtil.defaultName(iid))
-        fileNameLabel.setLabel(info.getFile().name)
+        val name = info.getFile().name
+
+        if (name.isEmpty()) {
+            fileNameLabel.setLabel(InformationUtil.defaultName(iid))
+        } else {
+            fileNameLabel.setLabel(info.getFile().name)
+        }
     }
 }
