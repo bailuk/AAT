b78840437f257fd0263c81c087e33d691147095b
bailuk <bailu@bailu.ch>
2024-09-02 20:46:49 +0200

    Remove ContentSource

--- aat-lib/src/main/java/ch/bailu/aat_lib/dispatcher/source/EditorSource.kt
+++ aat-lib/src/main/java/ch/bailu/aat_lib/dispatcher/source/EditorSource.kt
@@ -4,8 +4,9 @@ import ch.bailu.aat_lib.app.AppContext
 import ch.bailu.aat_lib.broadcaster.AppBroadcaster
 import ch.bailu.aat_lib.broadcaster.BroadcastData
 import ch.bailu.aat_lib.broadcaster.BroadcastReceiver
-import ch.bailu.aat_lib.dispatcher.ContentSource
 import ch.bailu.aat_lib.dispatcher.EditorSourceInterface
+import ch.bailu.aat_lib.dispatcher.SourceInterface
+import ch.bailu.aat_lib.dispatcher.TargetInterface
 import ch.bailu.aat_lib.dispatcher.usage.UsageTrackerInterface
 import ch.bailu.aat_lib.gpx.GpxList
 import ch.bailu.aat_lib.gpx.information.GpxFileWrapper
@@ -14,13 +15,23 @@ import ch.bailu.aat_lib.service.editor.EditorHelper
 import ch.bailu.aat_lib.service.editor.EditorInterface
 import ch.bailu.foc.Foc
 
-class EditorSource(private val appContext: AppContext, usageTracker: UsageTrackerInterface) : ContentSource(),
+class EditorSource(private val appContext: AppContext, usageTracker: UsageTrackerInterface) : SourceInterface,
     EditorSourceInterface {
+
+    private var target = TargetInterface.NULL
     private val edit: EditorHelper = EditorHelper(appContext)
     private val onFileEdited = BroadcastReceiver { args ->
         if (BroadcastData.has(args, edit.vID)) {
             requestUpdate()
         }
+    }
+
+    override fun setTarget(target: TargetInterface) {
+        this.target = target
+    }
+
+    fun sendUpdate(iid: Int, info: GpxInformation) {
+        target.onContentUpdated(iid, info)
     }
 
     init {
