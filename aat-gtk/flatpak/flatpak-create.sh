ea0874b2b38681679d1b75339ed83d25adab3c42
bailuk <bailu@bailu.ch>
2025-02-10 09:43:27 +0100

    Adjust flatpak script and run config

--- aat-gtk/flatpak/flatpak-create.sh
+++ aat-gtk/flatpak/flatpak-create.sh
@@ -1,12 +1,21 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+set -e
 
 short_id="aat"
 full_id="ch.bailu.$short_id"
+build_path="build/flatpak"
 
-test -d flatpak || cd ..
-cd flatpak || exit 1
+# As described in https://docs.flathub.org/docs/for-app-authors/submission/
 
-mkdir -p build || exit 1
+flatpak_base="build/flatpak"
+flatpak_export="${flatpak_base}/export"
+flatpak_repo="${flatpak_base}/repo"
+flatpak_build="${flatpak_base}/build"
+
+mkdir -p ${flatpak_repo}
+mkdir -p ${flatpak_build}
+mkdir -p ${flatpak_export}
 
 export LANG=C # So we can google error messages
 
@@ -18,33 +27,49 @@ if [ ! -z $1 ]; then
 fi
 
 echo "_"
-echo "Install dependencies"
-flatpak install flathub org.gnome.Platform//44 org.gnome.Sdk//44 org.freedesktop.Sdk.Extension.openjdk17/x86_64/22.08 || exit 1
+echo "Add flathub repo for user"
+flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 echo "_"
-echo "Build flatpak"
-flatpak-builder --force-clean build/build "$full_id.json" || exit 1
+echo "Install flatpak builder"
+flatpak install --user flathub org.flatpak.Builder || exit 1
 
 echo "_"
-echo "Install flatpak"
-echo "flatpak-builder --user --install --force-clean build/build $full_id.json || exit 1"
+echo "Build and install flatpak"
+flatpak run org.flatpak.Builder \
+  --force-clean \
+  --sandbox \
+  --user \
+  --install \
+  --install-deps-from=flathub \
+  --ccache \
+  --mirror-screenshots-url=https://dl.flathub.org/media/ \
+  --repo=${flatpak_repo} \
+  ${flatpak_build} "$full_id.json"
 
 # Single-file bundle
 # https://unix.stackexchange.com/questions/695934/how-do-i-build-a-flatpak-package-file-from-a-flatpak-manifest
 # https://docs.flatpak.org/en/latest/single-file-bundles.html
 
 echo "_"
-echo "Export flatpak"
-flatpak build-export build/export build/build || exit 1
+echo "Lint manifest"
+flatpak run --command=flatpak-builder-lint org.flatpak.Builder manifest ch.bailu.aat.json || true
 
+echo "_"
+echo "Lint repo"
+flatpak run --command=flatpak-builder-lint org.flatpak.Builder repo ${flatpak_repo} || true
 
 echo "_"
 echo "Build single-file bundle"
-flatpak build-bundle build/export build/aat.flatpak "$full_id"
+echo "> flatpak build-bundle ${flatpak_export} ${flatpak_base}/${short_id}.flatpak "$full_id""
 
 echo "_"
-echo "Install single-file bundle"
-echo "> flatpak install build/$short_id.flatpak"
+echo "Export flatpak"
+echo "> flatpak build-export ${flatpak_export} ${flatpak_build}"
+
+echo "_"
+echo "Install system-wide"
+echo "> flatpak install --system ./${flatpak_export}  ${full_id}"
 
 echo "_"
 echo "Run flatpak"
