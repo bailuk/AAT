e4c2c46503ec69979f61a10a5913bc874e839110
bailuk <bailu@bailu.ch>
2025-07-29 20:43:06 +0200

    Collect reports and publish test results

--- ci/collect-artifacts.sh
+++ ci/collect-artifacts.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
-mkdir -p dist/gtk || exit 1
-cp aat-gtk/build/libs/* dist/gtk || exit 1
-cp aat-gtk/util/install.sh dist/gtk || exit 1
-cp aat-gtk/gresource/icons/scalable/apps/ch.bailu.aat.svg dist/gtk || exit 1
+mkdir -p dist/gtk/aat-gtk || exit 1
+cp aat-gtk/build/libs/* dist/gtk/aat-gtk || exit 1
+cp aat-gtk/util/install.sh dist/gtk/aat-gtk || exit 1
+cp aat-gtk/gresource/icons/scalable/apps/ch.bailu.aat.svg dist/gtk/aat-gtk || exit 1
 
-mkdir -p dist/android || exit 1
-cp -r aat-android/build/outputs/apk/* dist/android || exit 1
+mkdir -p dist/android/aat-android || exit 1
+cp -r aat-android/build/outputs/apk/* dist/android/aat-android || exit 1
+
+mkdir -p dist/reports/aat-lib || exit 1
+mkdir -p dist/reports/aat-android || exit 1
+mkdir -p dist/reports/build || exit 1
+cp -r build/reports/* dist/build
+cp -r aat-android/build/reports/* dist/reports/aat-android
+cp -r aat-lib/build/reports/* dist/reports/aat-lib
