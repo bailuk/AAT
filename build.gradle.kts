64809afdfbb3f1bedabd4be20060ec394a2a0ab5
bailuk <bailu@bailu.ch>
2025-10-20 10:04:05 +0200

    CI: Update gradle shadow plugin to version 9.2.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
 
     id("org.jetbrains.kotlin.android") version "2.1.20" apply(false)
 
-    // https://imperceptiblethoughts.com/shadow/getting-started
-    id("com.gradleup.shadow") version "8.3.6" apply(false)
+    // https://github.com/GradleUp/shadow
+    id("com.gradleup.shadow") version "9.2.2" apply(false)
 
     // https://kotlinlang.org/docs/gradle-configure-project.html
     id("org.jetbrains.kotlin.jvm") version "2.1.20" apply(false)
