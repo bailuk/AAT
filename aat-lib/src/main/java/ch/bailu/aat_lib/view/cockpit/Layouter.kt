af07e50a6e194ed651d0963a8be2033b75f589a6
bailuk <bailu@bailu.ch>
2026-02-23 17:34:51 +0100

    Port Layouter to Kotlin

--- aat-lib/src/main/java/ch/bailu/aat_lib/view/cockpit/Layouter.kt
+++ aat-lib/src/main/java/ch/bailu/aat_lib/view/cockpit/Layouter.kt
@@ -1,124 +1,114 @@
-package ch.bailu.aat_lib.view.cockpit;
+package ch.bailu.aat_lib.view.cockpit
 
-import java.util.List;
+import ch.bailu.aat_lib.description.ContentDescription
+import kotlin.math.max
 
-import ch.bailu.aat_lib.description.ContentDescription;
+class Layouter(private val items: MutableList<ContentDescription>, doPlacement: DoPlacement) {
+    private var parentWidth = 0
+    private var parentHeight = 0
 
-public class Layouter {
-    private static final int MAX_CHARS_PER_LINE=50;
-    private int parent_width=0;
-    private int parent_height=0;
+    private val placer: Placer = Placer({ index: Int, x: Int, y: Int, w: Int, h: Int -> })
+    private val realPlacer: Placer = Placer(doPlacement)
 
-    private final Placer placer = new Placer((index, x, y, w, h) -> {});
-    private final Placer realPlacer;
+    fun layout(w: Int, h: Int) {
+        var chars = 1
 
-    private final List<ContentDescription> items;
-
-
-    public Layouter(List<ContentDescription> items, DoPlacement doPlacement) {
-        this.items = items;
-        this.realPlacer = new Placer(doPlacement);
-    }
-
-    public void layout(int w, int h) {
-        int chars = 1;
-
-        parent_width = w;
-        parent_height = h;
+        parentWidth = w
+        parentHeight = h
 
         while (chars < MAX_CHARS_PER_LINE) {
             if (tryPlacement(chars)) {
-                doPlacement(chars);
-                break;
-            } else chars++;
+                doPlacement(chars)
+                break
+            } else chars++
         }
     }
 
-
-    private boolean tryPlacement(int charsPerLine) {
-        placer.place(charsPerLine);
-        return placeItems(placer);
+    private fun tryPlacement(charsPerLine: Int): Boolean {
+        placer.place(charsPerLine)
+        return placeItems(placer)
     }
 
-    private boolean doPlacement(int charsPerLine) {
-        realPlacer.place(charsPerLine);
-        return placeItems(realPlacer);
+    private fun doPlacement(charsPerLine: Int): Boolean {
+        realPlacer.place(charsPerLine)
+        return placeItems(realPlacer)
     }
 
-    private boolean placeItems(Placer p) {
-        boolean works=true;
+    private fun placeItems(p: Placer): Boolean {
+        var works = true
 
-        final int size = items.size();
-        for (int i=0; i< size && works; i++) {
-            works = p.placeItem(i);
+        val size = items.size
+        var i = 0
+        while (i < size && works) {
+            works = p.placeItem(i)
+            i++
         }
-        return works;
+        return works
     }
 
-    private class Placer {
-        private static final int MIN_CHARS = 4;
-        private static final int RATIO=3;
-        private int char_width, char_height, xpos, ypos;
+    private inner class Placer(private val doPlacement: DoPlacement) {
+        private var charWidth = 0
+        private var charHeight = 0
+        private var xPos: Int = 0
+        private var yPos: Int = 0
 
-        private final DoPlacement doPlacement;
-
-        public Placer(DoPlacement doPlacement) {
-            this.doPlacement = doPlacement;
+        fun place(charsPerLine: Int) {
+            this.yPos = 0
+            this.xPos = 0
+            calculateCharGeometry(charsPerLine)
         }
 
-        public void place(int charsPerLine) {
-            xpos=ypos=0;
-            calculateCharGeometry(charsPerLine);
-        }
+        fun calculateCharGeometry(charsPerLine: Int) {
+            charWidth = parentWidth / charsPerLine
 
-        private void calculateCharGeometry(int charsPerLine) {
-            char_width=parent_width/charsPerLine;
-
-            char_height=char_width*RATIO;
-            if (char_height > parent_height) {
-                char_height=parent_height;
-                char_width=char_height/RATIO;
+            charHeight = charWidth * RATIO
+            if (charHeight > parentHeight) {
+                charHeight = parentHeight
+                charWidth = charHeight / RATIO
             }
         }
 
-        public boolean placeItem(int index) {
-            boolean works=true;
-            int width=getWidthOfView(index);
+        fun placeItem(index: Int): Boolean {
+            var works = true
+            val width = getWidthOfView(index)
 
-            if (width>parent_width) {
-                works=false;
-            } else if (width+xpos > parent_width) {
-                works=addLine();
-                if (works) works = placeItem(index);
+            if (width > parentWidth) {
+                works = false
+            } else if (width + this.xPos > parentWidth) {
+                works = addLine()
+                if (works) works = placeItem(index)
             } else {
-                setGeometry(index, width, char_height);
-                xpos += width;
+                setGeometry(index, width, charHeight)
+                this.xPos += width
             }
-            return works;
+            return works
         }
 
-        protected void setGeometry(int index, int width, int height) {
-            doPlacement.setGeometry(index, getXPos(),getYPos(), getXPos()+width, getYPos()+height);
+        private fun setGeometry(index: Int, width: Int, height: Int) {
+            doPlacement.setGeometry(index, this.xPos, this.yPos, this.xPos + width, this.yPos + height)
         }
 
-        protected int getXPos() {return xpos;}
-        protected int getYPos() {return ypos;}
-
-        private boolean addLine() {
-            ypos+=char_height;
-            xpos=0;
-            return ((char_height + ypos) <= parent_height);
+        fun addLine(): Boolean {
+            this.yPos += charHeight
+            this.xPos = 0
+            return ((charHeight + this.yPos) <= parentHeight)
         }
 
-
-        private int getWidthOfView(int index) {
-            int len= items.get(index).getValue().length();
-            len = Math.max(len, MIN_CHARS);
-            return len*char_width;
+        fun getWidthOfView(index: Int): Int {
+            var len = items[index].getValue().length
+            len = max(len, MIN_CHARS)
+            return len * charWidth
         }
     }
 
-    public interface DoPlacement {
-        void setGeometry(int index, int x, int y, int x2, int y2);
+    fun interface DoPlacement {
+        fun setGeometry(index: Int, x: Int, y: Int, x2: Int, y2: Int)
+    }
+
+    companion object {
+        private const val MAX_CHARS_PER_LINE = 50
+        private const val MIN_CHARS = 4
+        private const val RATIO = 3
+
     }
 }
