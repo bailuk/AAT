7a3a66e62cd5ec88404d5b05c5155b0c7564ac7d
bailuk <bailu@bailu.ch>
2026-02-23 17:57:34 +0100

    Port AltitudeColorTable to Kotlin

--- aat-lib/src/main/java/ch/bailu/aat_lib/map/layer/gpx/painter/RoutePainterNode.kt
+++ aat-lib/src/main/java/ch/bailu/aat_lib/map/layer/gpx/painter/RoutePainterNode.kt
@@ -9,11 +9,12 @@ class RoutePainterNode(private val mcontext: MapContext, private val color: Int,
     : GpxListPainter(mcontext, minPixelSpace) {
     override fun drawEdge(nodes: TwoNodes) {}
     override fun drawNode(node: TwoNodes.PixelNode) {
-        val c: Int
         val altitude = node.point.getAltitude()
-        c =
-            if (altitude == ElevationProvider.NULL_ALTITUDE) color else AltitudeColorTable.instance()
-                .getColor(altitude.toInt())
-        mcontext.draw().bitmap(mcontext.draw().getNodeBitmap(), node.pixel, c)
+        val altitudeColor = if (altitude == ElevationProvider.NULL_ALTITUDE) {
+            color
+        } else {
+            AltitudeColorTable.getColor(altitude.toInt())
+        }
+        mcontext.draw().bitmap(mcontext.draw().getNodeBitmap(), node.pixel, altitudeColor)
     }
 }
